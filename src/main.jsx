8e364e1f676fa3ab19cbd66415109c050f11f97c
Vishal Kulria <153205128+vishalkulria@users.noreply.github.com>
2026-04-16 21:50:38 +0530

    Add comment to main entry point

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import App from './App'
 import { store } from './store/store'
 import './index.css'
 
+// Application entry point with Redux and Router setup
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
